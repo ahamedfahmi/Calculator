f46b1a05be947d0f67a4083546984de8c61624f9
Ahamed Fahmi <dev.ahamedfahmi@gmail.com>
2021-11-04 16:43:58 +0530

    Added some functions not a working product till now

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var num1 : EditText
+    lateinit var num2 : EditText
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+         num1 = findViewById<EditText>(R.id.input_text)
+         num2 = findViewById<EditText>(R.id.input_text1)
+
+
+        //operators
+
+        val add = findViewById<Button>(R.id.add)
+        val subtract = findViewById<Button>(R.id.subs)
+        val multiply = findViewById<Button>(R.id.multiply)
+        val divide = findViewById<Button>(R.id.divide)
+
+        //equal button
+
+        val eqBtn = findViewById<Button>(R.id.btn_equal)
+
+        eqBtn.setOnClickListener{
+
+            if (add.isPressed){
+
+            }
+
+
+        }
+
+
     }
+
 }
